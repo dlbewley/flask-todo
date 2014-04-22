863e25d35cbc74097c3111be77ad777415e2643f
Dale Bewley <dale@stinky.bewley.net>
2014-04-21 21:33:15 -0700

    logins

--- wsgi/todoapp.py
+++ wsgi/todoapp.py
@@ -9,6 +9,15 @@ app.config.from_pyfile('todoapp.cfg')
 app.config['PROPAGATE_EXCEPTIONS'] = True
 db = SQLAlchemy(app)
 
+from flask.ext.login import LoginManager
+ 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Todo(db.Model):
     __tablename__ = 'todos'
@@ -24,6 +33,34 @@ class Todo(db.Model):
         self.done = False
         self.pub_date = datetime.utcnow()
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id',db.Integer , primary_key=True)
+    username = db.Column('username', db.String(20), unique=True , index=True)
+    password = db.Column('password' , db.String(10))
+    email = db.Column('email',db.String(50),unique=True , index=True)
+    registered_on = db.Column('registered_on' , db.DateTime)
+ 
+    def __init__(self , username ,password , email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return unicode(self.id)
+ 
+    def __repr__(self):
+        return '<User %r>' % (self.username)
 
 @app.route('/')
 def index():
@@ -58,6 +95,21 @@ def show_or_update(todo_id):
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.route('/register' , methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'] , request.form['password'],request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+ 
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
