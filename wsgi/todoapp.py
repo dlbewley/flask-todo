00afea9399f8f6b1845c5233dd3f696f5ede64bb
Dale Bewley <dale@stinky.bewley.net>
2014-04-22 20:37:53 -0700

    reorder

--- wsgi/todoapp.py
+++ wsgi/todoapp.py
@@ -3,49 +3,6 @@ from flask.ext.login import LoginManager, login_user, logout_user, current_user,
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-class Todo(db.Model):
-    __tablename__ = 'todos'
-    id            = db.Column('todo_id', db.Integer, primary_key=True)
-    title         = db.Column(db.String(60))
-    text          = db.Column(db.String)
-    done          = db.Column(db.Boolean)
-    pub_date      = db.Column(db.DateTime)
-
-    def __init__(self, title, text):
-        self.title    = title
-        self.text     = text
-        self.done     = False
-        self.pub_date = datetime.utcnow()
-
-
-class User(db.Model):
-    __tablename__ = "users"
-    id            = db.Column('user_id', db.Integer, primary_key=True)
-    username      = db.Column('username', db.String(20), unique=True, index=True)
-    password      = db.Column('password', db.String(10))
-    email         = db.Column('email', db.String(50), unique=True, index=True)
-    registered_on = db.Column('registered_on', db.DateTime)
- 
-    def __init__(self , username ,password , email):
-        self.username      = username
-        self.password      = password
-        self.email         = email
-        self.registered_on = datetime.utcnow()
-
-    def is_authenticated(self):
-        return True
- 
-    def is_active(self):
-        return True
- 
-    def is_anonymous(self):
-        return False
- 
-    def get_id(self):
-        return unicode(self.id)
- 
-    def __repr__(self):
-        return '<User %r>' % (self.username)
 
 #-------------------------------------------------------
 app = Flask(__name__)
@@ -111,6 +68,50 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     return redirect(url_for('index'))
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id            = db.Column('todo_id', db.Integer, primary_key=True)
+    title         = db.Column(db.String(60))
+    text          = db.Column(db.String)
+    done          = db.Column(db.Boolean)
+    pub_date      = db.Column(db.DateTime)
+
+    def __init__(self, title, text):
+        self.title    = title
+        self.text     = text
+        self.done     = False
+        self.pub_date = datetime.utcnow()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id            = db.Column('user_id', db.Integer, primary_key=True)
+    username      = db.Column('username', db.String(20), unique=True, index=True)
+    password      = db.Column('password', db.String(10))
+    email         = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+ 
+    def __init__(self , username ,password , email):
+        self.username      = username
+        self.password      = password
+        self.email         = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return unicode(self.id)
+ 
+    def __repr__(self):
+        return '<User %r>' % (self.username)
 
 if __name__ == '__main__':
     app.run()
