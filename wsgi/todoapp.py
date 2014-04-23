43ca1306b6a18ae9296dc77928e21d48a6774bb9
Dale Bewley <dale@stinky.bewley.net>
2014-04-22 21:02:04 -0700

    add missing login code

--- wsgi/todoapp.py
+++ wsgi/todoapp.py
@@ -109,14 +109,19 @@ def register():
     flash('User successfully registered')
     return redirect(url_for('login'))
  
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
-    # login and validate the user...
-    login_user(user)
-    flash("Logged in successfully.")
-    return redirect(request.args.get("next") or url_for("index"))
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(username=username,password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
 
 if __name__ == '__main__':
     app.run()
